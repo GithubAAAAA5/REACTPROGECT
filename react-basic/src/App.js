c58c1cf33875140711fc2fab9af44300621c60e4
gusrhks <gusrhks712@gmail.com>
2024-06-11 16:49:00 +0900

    240611 ed

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { Fragment } from 'react';
+import Wrapper from './Wrapper';
+import PropTypes from 'prop-types'; // prop 타입 검증을 모듈.
 
 function App() {
+
+  const name = 'React';
+
+  const style = {
+    backgroundColor: 'red', // 카멜 표기법
+    color: 'white',
+    fontSize: 24,
+    padding: '1rem'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Wrapper>
+    <MyComponent name={"홍길동"} age={20} email={"Hong@naver.com"} isSpecial={true}/>
+    <div className='test-box'>
+      <WelcomeFunction name="Sara" />
+      <WelcomeClass name="Cahal" />
+      <WelcomeFunction name="Edite" />
+      <WelcomeFunction name={name} />
+      <br /> {/* <br></br> 태그는 닫아야한다. */}
     </div>
+    <div style={style}>Hello World!!</div>  {/* 하나의 태그로 묶여야 한다. */}
+    </Wrapper>
+    </>
   );
 }
 
+// 함수형 컴포넌트
+function WelcomeFunction(props) {
+  console.log(props);
+  return <h1>Hello, {props.name}</h1>;
+}
+
+// 클래스형 컴포넌트
+class WelcomeClass extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+
+// 화살표 함수를 이용한 컴포넌트
+const MyComponent = ({name, age, addr=' 서울시 신촌', email, isSpecial}) => {
+  return (
+    <>
+      { isSpecial && // 3항 연산식
+      <div>
+        나의 첫번째 MyComponent 입니다.<br />
+        프로퍼티 1 : {name}<br />
+        프로퍼티 2 : {age}<br />
+        기본값 프로퍼티 3 : {addr}<br />
+        </div>
+      }
+    </>
+  );
+}
+
+// props 기본값 설정하기.. 중요* 대소문자를 구분함
+//  컴포넌트에 props 를 지정하지 않았을 때 기본적으로 사용할 값을 설정하는 경우에 사용함
+//  syntax : 컴포넌트.defaultProps = {}
+MyComponent.defaultProps = {    // .defaultProps 를 통해 값설정
+  addr : '서울시 신촌',
+  age : 0
+}
+
+// props 타입 검증 ( 주의 사항 - 대소문자를 정확하게 구분한다 )
+// npm install prop-types
+// 컴포넌트명.propTypes = {}
+// PropType 이 기대하는 값을 가지는지 여부를 확인한다.
+MyComponent.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number,
+  email: PropTypes.string.isRequired    // 필수 값( 상위 컴포넌트에서 변경)
+}
 export default App;
---
+++ react-basic/src/Wrapper.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+
+/* props.child 
+    -> 컴포넌트 태그 사이에 넣은 값을 조회하고 싶을 때, props.children 을 조회해서 사용
+*/
+// function Wrapper({children}) {
+//     const style = {
+//         border: '2px solid black',
+//         padding: '16px',
+//     };
+
+//     console.log(children);
+
+//     return (
+//         <div style={style}>
+//             {children}
+//         </div>
+
+function Wrapper(props) {
+    const style = {
+        border: '2px solid black',
+        padding: '16px',
+    };
+
+    console.log(props);
+
+    return (
+        <div style={style}>
+            {props.children}    {/* children 으로 들어온 컴포넌트를 랜더링에 추가함 */}
+        </div>
+    );
+}
+
+export default Wrapper;
