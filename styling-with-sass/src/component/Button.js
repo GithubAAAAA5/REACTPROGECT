ddf605a90aaa9efff5a5a9f62e21a9f23db92eb6
gusrhks <gusrhks712@gmail.com>
2024-06-14 12:49:37 +0900

    240614 3rd

--- styling-with-sass/src/component/Button.js
+++ styling-with-sass/src/component/Button.js
@@ -1,9 +1,18 @@
 // Button
 import React from 'react';
 import './Button.scss';
-
-function Button({children}) {
-    return <button className='Button'>{children}</button>
+import classNames from 'classnames';
+// 1. 버튼 생성....
+// 2. 버튼 사이즈 조정하기.... (large, medium, small)
+// 3. 버튼 색상 설정하기 ... (blue, gray, pink)
+function Button({children, size}) {
+    // className='Button large'
+    // ['Button',size].join(' ') or `Button ${size}`
+    // 클래스에 관련된 처리를 쉽게하는 라이브러리.... classnames
+    return <button className={classNames('Button', size)}>{children}</button>
 }
+Button.defaultProps = {
+    size: 'medium'
+};
 
 export default Button;
