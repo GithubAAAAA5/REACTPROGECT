7fa0555e1a7a4eb5c12b22ea6009246e05507330
gusrhks <gusrhks712@gmail.com>
2024-06-14 14:35:53 +0900

    240614 4th

--- styling-with-sass/src/App.js
+++ styling-with-sass/src/App.js
@@ -10,6 +10,31 @@ function App() {
         <Button>Button</Button>
         <Button size={"small"}>Button</Button>
       </div>
+
+      <div className='button'>
+        <Button size="large" color="gray">Button</Button>
+        <Button color="gray">Button</Button>
+        <Button size={"small"} color={"gray"}>Button</Button>
+      </div>
+
+      <div className='button'>
+        <Button size="large" color="pink">Button</Button>
+        <Button color="pink">Button</Button>
+        <Button size={"small"} color={"pink"}>Button</Button>
+      </div>
+
+      <div className='button'>
+        <Button size="large" color="blue" outline>Button</Button>
+        <Button color="gray" outline>Button</Button>
+        <Button size={"small"} color={"pink"} outline>Button</Button>
+      </div>
+
+      <div className='button'>
+        <Button size="large" color="blue" fullWidth>Button</Button>
+        <Button size="medium" color="gray" fullWidth>Button</Button>
+        <Button size="small" color="pink" fullWidth>Button</Button>
+      </div>
+
     </div>
   );
 }
--- styling-with-sass/src/component/Button.js
+++ styling-with-sass/src/component/Button.js
@@ -5,14 +5,22 @@ import classNames from 'classnames';
 // 1. 버튼 생성....
 // 2. 버튼 사이즈 조정하기.... (large, medium, small)
 // 3. 버튼 색상 설정하기 ... (blue, gray, pink)
-function Button({children, size}) {
+// 4. outline 옵션 : 버튼에서 테두리만 보여지게 설정 outline에 {}를 넣어주는 이유는 toggle 처럼 사용하기 위함
+// 5. fullWidth : 버튼이 전체 너비를 차지하도록 구현
+function Button({children, size, color, outline, fullWidth}) {
     // className='Button large'
     // ['Button',size].join(' ') or `Button ${size}`
     // 클래스에 관련된 처리를 쉽게하는 라이브러리.... classnames
-    return <button className={classNames('Button', size)}>{children}</button>
+    return (
+        <button className={classNames('Button', size, color, {outline, fullWidth})}>
+            {children}
+        </button>
+        
+    );
 }
 Button.defaultProps = {
-    size: 'medium'
+    size: 'medium',
+    color: 'blue'
 };
 
 export default Button;
